8b16002dcdd3ed2d8b43a3e3be7442270a901d2c
Gizmocreature <Gizmocreature@9f3bcd65-5856-0410-9c14-b544f31f038b>
2008-12-19 05:01:16 +0000

    Added barsprings branch. I'm not very used to SVN...

---
+++ branches/barsprings/BarSpring.java
@@ -0,0 +1,114 @@
+import java.awt.*;
+import java.math.*;
+public class BarSpring extends PhysObject {
+    public Mass ma, mb;
+    public double restlength;
+    private double k = 1;
+    public double dist(int mx, int my) {
+        try {
+            double ya = ma.y;
+            double yb = mb.y;
+            double xa = ma.x;
+            double xb = mb.x;
+            double S = (yb-ya)/(xb-xa);
+            double ptx = (my-ya+mx/S + S*xa)/(S+1/S);
+            double pty = ya + S*(ptx-xa);
+            double maxx = xa;
+            double minx = xb;
+            double maxy = ya;
+            double miny = yb;
+            if(maxx < minx) {
+                maxx = xb;
+                minx = xa;
+            }
+            if(maxy < miny) {
+                maxy = yb;
+                miny = ya;
+            }
+            if(ptx > maxx) {
+                ptx = maxx;
+            }
+            if(ptx < minx) {
+                ptx = minx;
+            }
+            if(pty > maxy) {
+                pty = maxy;
+            }
+            if(pty < miny) {
+                pty = miny;
+            }
+            System.out.println(ptx + " " + pty + " ");
+            return Math.sqrt((mx-ptx)*(mx-ptx)+(my-pty)*(my-pty));
+        } catch(Exception e) {System.out.println(e.getMessage());}
+        return 0;
+    }
+    public double massdist() {
+        return Math.sqrt((ma.x-mb.x)*(ma.x-mb.x) + (ma.y-mb.y)*(ma.y-mb.y));
+    }
+    public void move() {/*
+        ForceVector fv = new ForceVector(ma.x-mb.x, ma.y-mb.y);
+        fv.setM(.5*env[4]*(massdist()-restlength)/stepmult);
+        ForceVector fvb = new ForceVector(mb.x-ma.x, mb.y-ma.y);
+        fvb.setM(.5*env[4]*(massdist()-restlength)/stepmult);
+        ma.addBarSpring(fvb);
+        mb.addBarSpring(fv);*/
+    }
+    public void interact(PhysObject po) {}
+    public void paintObject(Graphics g) {
+        if(!ma.exists || !mb.exists) {
+            this.exists = false;
+            return;
+        }
+        double drawXa = ma.x;
+        double drawYa = ma.y;
+        if(drawXa > env[3]) {
+            drawXa = env[3];
+        } else if (drawXa < 0 ) {
+            drawXa = 0;
+        }
+        if(drawYa > env[2]) {
+            drawYa = env[2];
+        } else if(drawYa < 0) {
+            drawYa = 0;
+        }
+        double drawXb = mb.x;
+        double drawYb = mb.y;
+        if(drawXb > env[3]) {
+            drawXb = env[3];
+        } else if (drawXb < 0 ) {
+            drawXb = 0;
+        }
+        if(drawYb > env[2]) {
+            drawYb = env[2];
+        } else if(drawYa < 0) {
+            drawYb = 0;
+        }
+        g.setColor(Color.black);
+        if(selected) {
+            g.setColor(Color.red);
+        }
+        g.drawLine((int)drawXa+3, (int)drawYa+3,(int)drawXb+3,(int)drawYb+3);
+    }
+    public void setEnv(double[] input) {
+        env = input;
+    }
+    public BarSpring(int xc, int xd, int yc, int yd) {
+        ma = new Mass(xc, yc);
+        mb = new Mass(xd, yd);
+    }
+    public BarSpring(Mass a, Mass b) {
+        ma = a;
+        mb = b;
+        restlength = massdist();
+    }
+    public BarSpring() {}
+    public void updateExists() {
+        try {
+            if(!ma.exists || !mb.exists) {
+                this.exists = false;
+                return;
+            }
+        } catch (Exception e) {this.exists = false;}
+    }
+        
+}
---
+++ branches/barsprings/Simulator.java
@@ -0,0 +1,166 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.awt.image.*;
+/**
+ * Simulator applet inspired by Ed Burton's sodaconstructor
+ * @author Colin Stanfill
+ * @version 3.1.4
+ */
+public class Simulator extends JPanel implements  Runnable, MouseListener, MouseMotionListener, KeyListener{
+    private double[] env = new double[5];
+    /**
+     * Class fields defined in Simulator:
+     * simthread = refresh thread
+     * iters = frame count
+     * canvas = drawing surface for everything
+     * buff and buffG = buffered graphics variables
+     */
+    private Thread simthread, canvasthread;
+    private int iters = 0;
+    private Canvas canvas;
+    private Graphics2D buffG;
+    private BufferedImage buff;
+    private double lastTime = 0;
+    private int sinceReset = 0;
+    private double fps = 0;
+    private boolean dispFPS = false;
+    /**
+     * Initialize the applet.
+     */
+    public Simulator() {   
+        setFocusable(true);
+        canvas = new Canvas(700, 500);
+        add(canvas);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        addKeyListener(this);
+        simthread = new Thread(this);
+        canvasthread = new Thread(canvas);
+        simthread.start();
+        this.setBackground(Color.white);
+        lastTime = System.currentTimeMillis();
+    }
+    /**
+     * Iterate the physics, call repaint().
+     */
+    public void run() {
+        while(true) {
+            repaint();
+            try {
+                canvas.changeSize(getHeight(), getWidth());
+                simthread.sleep(10);
+                canvasthread.run();
+                canvasthread.join();
+                
+                //this.iters++;
+            } catch(Exception ie) {}
+            //System.out.println(iters + "!!");
+        }
+    }
+    /**
+     * Paint the drawing surface on the applet.
+     * @param   g   A graphics object
+     */
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        sinceReset++;
+        if(dispFPS) {
+            g.drawString(""+fps,50,50);
+        }
+        if(sinceReset == 50) {
+            fps = ((int)(sinceReset*100000/(System.currentTimeMillis()-lastTime)))/100.0;
+            lastTime = System.currentTimeMillis();
+            sinceReset = 0;
+        }
+    }
+    /**
+     * Update the graphics by just painting
+     * @param   g   A graphics object
+     */
+    public void update(Graphics g) {
+        paint(g);
+    }
+    /**
+     * Does nothing
+     * @param   me  A mouse event.
+     */
+    public void mouseExited(MouseEvent me) {}
+    /**
+     * Does nothing
+     * @param   me  A mouse event.
+     */
+    public void mouseEntered(MouseEvent me) {}
+    /**
+     * Wrapper for the canvas' mousePress method
+     * @param   me  A mouse event.
+     */
+    public void mousePressed(MouseEvent me) {
+        canvas.mousePress(me.getX(), me.getY(), (me.getButton() == me.BUTTON3));
+    }
+    /**
+     * Wrapper for the canvas' mouseRelease method
+     * @param   me  A mouse event.
+     */
+    public void mouseReleased(MouseEvent me) {
+        canvas.mouseRelease(me.getX(), me.getY(), (me.getButton() == me.BUTTON3));
+    }
+    /**
+     * Does nothing
+     * @param   me  A mouse event.
+     */
+    public void mouseClicked(MouseEvent me) {}
+    
+    /**
+     * Wrapper for the canvas' mouseMoved method
+     * @param   me  A mouse motion event.
+     */
+    public void mouseMoved(MouseEvent me) {
+        canvas.mouseMove(me.getX(), me.getY());
+    }
+    /**
+     * Wrapper for the canvas' mouseDragged method
+     * @param   me  A mouse motion event.
+     */
+    public void mouseDragged(MouseEvent me) {
+        canvas.mouseDrag(me.getX(), me.getY(), (me.getButton() == me.BUTTON3));
+    }
+    /**
+     * Wrapper for the canvas' keyPress method
+     * @param   ke  A keyboard event.
+     */
+    public void keyPressed(KeyEvent ke) {
+        canvas.keyPress(ke.getKeyCode());
+    }
+    /**
+     * Wrapper for the canvas' keyRelease method
+     * @param   ke  A keyboard event.
+     */
+    public void keyReleased(KeyEvent ke) {
+        canvas.keyRelease(ke.getKeyCode());
+    }
+    /**
+     * Wrapper for the canvas' keyType method
+     * @param   ke  A keyboard event.
+     */
+    public void keyTyped(KeyEvent ke) {
+        canvas.keyType(ke.getKeyCode());
+    }    
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Gizmoconstructor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Simulator c = new Simulator();
+        c.setOpaque(true); 
+        frame.setContentPane(c);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
